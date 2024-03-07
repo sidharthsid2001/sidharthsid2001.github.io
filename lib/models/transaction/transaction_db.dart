b7349063398a0606fe3cc5c4f64614cf9a4b3c5a
Sidharth A S <sidharthas@ults.ulccs>
2024-03-07 10:53:07 +0530

    second commit

--- lib/models/transaction/transaction_db.dart
+++ lib/models/transaction/transaction_db.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:money_manager/db/transactins/transaction_model.dart';
